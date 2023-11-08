f30195bfe1beb539a09dc9e93ba9527cbcb05100
Kevin Hindriks <kevinhindriks@gmail.com>
2023-11-09 01:35:57 +0800

    test commit

--- main.lua
+++ main.lua
@@ -1,1 +1,13 @@
--- test
+function love.load()
+    wf = require '/libaries/windfield/windfield'
+    world = wf.newWorld(0, 0)
+end
+
+
+function love.update(dt)
+
+end
+
+function love.draw()
+
+end
